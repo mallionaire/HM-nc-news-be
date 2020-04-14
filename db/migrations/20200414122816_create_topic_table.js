708392087c2a68cfc76ccc6d7ed064f28262eb1a
mallionaire <hjmallion@gmail.com>
2020-04-14 14:44:16 +0100

    initial setup and migrations folder added

---
+++ db/migrations/20200414122816_create_topic_table.js
@@ -0,0 +1,18 @@
+exports.up = function (knex) {
+  console.log("Creating TOPIC table");
+  //
+  return knex.schema.createTable("topics", (topicTable) => {
+    topicTable.text("slug").notNullable().primary().unique();
+    topicTable.text("description") /*.notNullable()*/;
+  });
+};
+
+exports.down = function (knex) {
+  console.log("Removing TOPIC table");
+  return knex.schema.dropTable("topics");
+};
+
+/*
+- `slug` field which is a unique string that acts as the table's primary key
+- `description` field which is a string giving a brief description of a given topic
+*/
---
+++ db/migrations/20200414122851_create_comment_table.js
@@ -0,0 +1,28 @@
+exports.up = function (knex) {
+  console.log("Creating COMMENT table");
+
+  return knex.schema.createTable("comments", (commentTable) => {
+    commentTable.increments("comment_id").primary();
+    commentTable.text("author").references("users.username");
+    commentTable.integer("article_id").references("articles.article_id");
+    commentTable.integer("votes").defaultTo(0);
+    commentTable.timestamp("created_at");
+    commentTable.text("body").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  console.log("Removing COMMENTS table");
+  return knex.schema.dropTable("comments");
+};
+
+//notNullable();
+
+/*
+- `comment_id` which is the primary key
+- `author` field that references a user's primary key (username)
+- `article_id` field that references an article's primary key
+- `votes` defaults to 0
+- `created_at` defaults to the current timestamp
+- `body`
+*/
