e3f5d3cb359ea33707a94e86093048364417af9c
mallionaire <hjmallion@gmail.com>
2020-04-14 16:08:59 +0100

    finished testing formatDates function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,19 @@
-exports.formatDates = list => {};
+exports.formatDates = (list) => {
+  return list.map(({ created_at, ...listItem }) => {
+    const newItem = {
+      created_at: new Date(created_at),
+      ...listItem,
+    };
+    return newItem;
+  });
+};
 
-exports.makeRefObj = list => {};
+/* 
+This utility function should be able to take an array (`list`) of objects and return a new array. Each item in the new array must have its timestamp converted into a Javascript date object. Everything else in each item must be maintained.
+
+_hint: Think carefully about how you can test that this has worked - it's not by copying and pasting a sql timestamp from the terminal into your test_
+*/
+
+exports.makeRefObj = (list) => {};
 
 exports.formatComments = (comments, articleRef) => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,85 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
   formatComments,
-} = require('../db/utils/utils');
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  it("returns an empty array when passed an empty array", () => {
+    expect(formatDates([])).to.be.an("array");
+  });
+  it("returns a new array", () => {
+    const inputArr = [];
+    const actual = formatDates(inputArr);
+    expect(actual).to.deep.equal([]);
+    expect(actual).to.not.equal(inputArr);
+  });
+  // it("returns an array of objects", () => {
+  //   const inputArr = [{}, {}, {}];
+  //   const actual = formatDates(inputArr);
+  //   expect(actual).to.deep.equal(inputArr);
+  //   actual.forEach((item) => {
+  //     expect(item).to.be.an("object");
+  //   });
+  // });
+  it("converts the created_at key for one list object into a JS date object", () => {
+    const inputArr = [
+      {
+        topic: "test topic",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+    ];
+    const actual = formatDates(inputArr);
+    expect(actual).to.deep.equal([
+      {
+        topic: "test topic",
+        created_at: new Date(1542284514171),
+        votes: 100,
+      },
+    ]);
+  });
+  it("converts the created_at key for an array of many objects into a JS date object", () => {
+    const inputArr = [
+      {
+        topic: "test topic",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+      {
+        topic: "test topic 2",
+        created_at: 1289996514171,
+        votes: 200,
+      },
+      {
+        topic: "test topic 3",
+        created_at: 1163852514171,
+        votes: 300,
+      },
+    ];
+    const expected = [
+      {
+        topic: "test topic",
+        created_at: new Date(1542284514171),
+        votes: 100,
+      },
+      {
+        topic: "test topic 2",
+        created_at: new Date(1289996514171),
+        votes: 200,
+      },
+      {
+        topic: "test topic 3",
+        created_at: new Date(1163852514171),
+        votes: 300,
+      },
+    ];
+    const actual = formatDates(inputArr);
+    expect(expected).to.deep.equal(actual);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
